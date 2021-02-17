631355df67d5670609174af0f6ff02be3bcf9d80
MrFengHa <1109179567@qq.com>
2021-02-17 23:53:16 +0800

    生成角色实体类和mapper文件

---
+++ admin-parent/admin-entity/src/main/java/com/home/entity/Role.java
@@ -0,0 +1,33 @@
+package com.home.entity;
+
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+/**
+ * 角色实体类
+ *
+ * @author 冯根源
+ * @date 2021/1/24 14:03
+ */
+@NoArgsConstructor
+@ToString
+public class Role {
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+}
